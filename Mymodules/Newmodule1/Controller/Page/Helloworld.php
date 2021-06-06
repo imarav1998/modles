2a9b00689933159fe9156f177f2796c10fcba0c4
imarav1998 <ponaravind1998@gmail.com>
2021-06-05 22:07:51 -0700

    master

---
+++ Mymodules/Newmodule1/Controller/Page/Helloworld.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Mymodules\Newmodule1\Controller\Page;
+
+
+class Helloworld extends \Magento\Framework\App\Action\Action
+{
+
+    protected $_pageFactory;
+    public function __construct(
+        \Magento\Framework\App\Action\Context $context,
+        \Magento\Framework\View\Result\PageFactory $pageFactory
+    ) {
+        $this->_pageFactory = $pageFactory;
+        return parent::__construct($context);
+    }
+    public function execute()
+    {
+        echo "Hello world";
+        exit;
+    }
+}
---
+++ Mymodules/Newmodule1/registration.php
@@ -0,0 +1,5 @@
+<?php	
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(ComponentRegistrar::MODULE,'Mymodule_Newmodule1', __DIR__);
