2f9cee0f5ca892c5a43e706258a58b1961407860
imarav1998 <ponaravind1998@gmail.com>
2021-06-06 10:32:08 -0700

    corrected module

--- Mymodules/Newmodule1/registration.php
+++ Mymodules/Newmodule1/registration.php
@@ -2,4 +2,4 @@
 
 use Magento\Framework\Component\ComponentRegistrar;
 
-ComponentRegistrar::register(ComponentRegistrar::MODULE,'Mymodule_Newmodule1', __DIR__);
+ComponentRegistrar::register(ComponentRegistrar::MODULE,'Mymodules_Newmodule1', __DIR__);
--- Pixelpro/Helloworld/registration.php
+++
@@ -1,6 +0,0 @@
-<?php
-\Magento\Framework\Component\ComponentRegistrar::register(
-	\Magento\Framework\Component\ComponentRegistrar::MODULE,
-	'Pixelpro_Helloworld',
-	__DIR__
-);
